ae5a4086a259def90a1b420d75cf2118f58f66ea
David Francis <David.Francis@amd.com>
2025-02-12 09:45:37 -0500

    files-ext: Allow plugin files to retry
    amdgpu dmabuf CRIU requires the ability of the amdgpu plugin
    to retry.
    
    Change files_ext.c to read a response of 1 from a plugin restore
    function to mean retry.
    
    Signed-off-by: David Francis <David.Francis@amd.com>

--- plugins/amdgpu/amdgpu_plugin.c
+++ plugins/amdgpu/amdgpu_plugin.c
@@ -1551,7 +1551,7 @@ exit:
 	return ret;
 }
 
-int amdgpu_plugin_restore_file(int id)
+int amdgpu_plugin_restore_file(int id, bool *retry_needed)
 {
 	int ret = 0, fd;
 	char img_path[PATH_MAX];
@@ -1561,6 +1561,8 @@ int amdgpu_plugin_restore_file(int id)
 	struct kfd_ioctl_criu_args args = { 0 };
 	size_t img_size;
 	FILE *img_fp = NULL;
+
+	*retry_needed = false;
 
 	if (plugin_disabled)
 		return -ENOTSUP;
